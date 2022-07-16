a15a2198b272e78f28d568cb1f2ae594c1240161
randronaco <randronaco1027@gmail.com>
2022-07-15 22:32:05 -0400

    added images and nav bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import Nav from './components/Nav';
+import Nav from './components/Nav';
 import About from './components/About';
 // import Portfolio from './components/Portfolio';
 // import Resume from './components/Resume'
@@ -7,14 +7,31 @@ import About from './components/About';
 
 
 
-function App() {
-  
-
+function App() {  
   return (
       <div>
+        <Nav></Nav>
         <main>
           <About></About>
         </main>
+        <footer>
+          <img
+            src={require(`./assets/github.png`)}
+            alt="GitHub Logo"
+            className="img-thumbnail mx-1"
+            key="GitHub Logo"
+            style={{width: 50, height: 50}}
+            href="https://github.com/randronaco1027"
+          />
+          <img
+            src={require(`./assets/linkedin.png`)}
+            alt="LinkedIn Logo"
+            className="img-thumbnail mx-1"
+            key="LinkedIn Logo"
+            style={{width: 50, height: 50}}
+            href="https://www.linkedin.com/in/ray-andronaco-jr-8729503a/"
+          />
+        </footer>
       </div>
     );
 }
---
+++ src/components/Nav/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function Nav(props) {
+  const {
+    contactSelected,
+    setContactSelected,
+  } = props;
+
+  return (
+    <header className="flex-row px-1">
+      <h2>
+        <a data-testid="link" href="/">
+          Ray Andronaco
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
+              About me
+            </a>
+          </li>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <span onClick={() => setContactSelected(true)}>Contact</span>
+          </li>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <span onClick={() => setContactSelected(true)}>Portfolio</span>
+          </li>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <span onClick={() => setContactSelected(true)}>Resume</span>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
